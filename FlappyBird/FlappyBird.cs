4e4f95653386143ceef2c20685ffad492721c280
its-Lyn <143884194+its-Lyn@users.noreply.github.com>
2024-09-05 17:03:44 +0300

    Add assets, create basic game window

--- FlappyBird/FlappyBird.cs
+++ FlappyBird/FlappyBird.cs
@@ -1,7 +1,66 @@
+using System.Numerics;
+
 namespace FlappyBird;
 
 public static class FlappyBird {
+    public const bool DEV_MODE = true;
+    
+    public static readonly string AssetPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets");
+    public static readonly Vector2 GameSize = new Vector2(288, 512);
+    
     public static void Main() {
-        Console.WriteLine("Hello, World!");
+        Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
+        Raylib.InitWindow((int)GameSize.X, (int)GameSize.Y, "Flappy Bird");
+        Raylib.SetWindowMinSize(144, 256);
+
+        // Create game world
+        RenderTexture2D renderer = Raylib.LoadRenderTexture((int)GameSize.X, (int)GameSize.Y);
+        Raylib.SetTextureFilter(renderer.Texture, TextureFilter.Point);
+
+        // Lock the game to 60 fps
+        // ...I can't be fucked to make DT physics
+        Raylib.SetTargetFPS(60);
+
+        Texture2D background = Raylib.LoadTexture(Path.Combine(AssetPath, "Sprites", "background-day.png"));
+
+        while (!Raylib.WindowShouldClose()) {
+            float scale = Math.Min(
+                Raylib.GetScreenWidth() / GameSize.X,
+                Raylib.GetScreenHeight() / GameSize.Y
+            );
+
+            // Drawing inside the game world
+            Raylib.BeginTextureMode(renderer);
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawTexture(
+                    background,
+                    0, 0, Color.White
+                );
+            Raylib.EndTextureMode();
+
+            // Drawing to the whole screen
+            Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Raylib.DrawTexturePro(
+                    renderer.Texture,
+                    new Rectangle(
+                        0, 0, renderer.Texture.Width, -renderer.Texture.Height
+                    ),
+                    new Rectangle(
+                        (Raylib.GetScreenWidth() - (GameSize.X * scale)) * 0.5f,
+                        (Raylib.GetScreenHeight() - (GameSize.Y * scale)) * 0.5f,
+                        GameSize.X * scale, GameSize.Y * scale
+                    ),
+                    Vector2.Zero,
+                    0,
+                    Color.White
+                );
+
+                if (DEV_MODE) Raylib.DrawFPS(5, 5);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.UnloadRenderTexture(renderer);
+        Raylib.CloseWindow();
     }
 }
