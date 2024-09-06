dde72b2eb2f3cf7175d39544d89ca7b384115ca7
its-Lyn <143884194+its-Lyn@users.noreply.github.com>
2024-09-06 17:13:48 +0300

    Add ground

---
+++ FlappyBird/Entities/Ground.cs
@@ -0,0 +1,27 @@
+using System.Numerics;
+
+namespace FlappyBird.Entities;
+
+public class Ground : Entity {
+    private readonly Texture2D Sprite = Raylib.LoadTexture(Path.Combine(FlappyBird.AssetPath, "Sprites", "base.png"));
+    private readonly float Speed = 2.0f;
+
+    private Vector2 _pos = Vector2.Zero;
+
+    public Ground(bool second) {
+        if (second) _pos.X = Sprite.Width;
+    }
+
+    public void Initialise()
+        => _pos.Y = FlappyBird.GameSize.Y - Sprite.Height;
+
+    public void Update() {
+        _pos.X -= Speed;
+        if (_pos.X < 0 - Sprite.Width) 
+            _pos.X = FlappyBird.GameSize.X; 
+    }
+ 
+    public void Draw() 
+        => Raylib.DrawTextureV(Sprite, _pos, Color.White);
+
+}
--- FlappyBird/States/Game.cs
+++ FlappyBird/States/Game.cs
@@ -4,17 +4,30 @@ namespace FlappyBird.States;
 
 public class Game : State {
     private readonly Bird _bird = new Bird();
+    private readonly Ground[] _ground = [
+        new Ground(false),
+        new Ground(true)
+    ];   
 
     public void Initialise() {
         _bird.Initialise();
+
+        foreach (Ground ground in _ground) 
+            ground.Initialise();
     }
 
     public void Update() {
         _bird.Update();
+ 
+        foreach (Ground ground in _ground) 
+            ground.Update();
     }
 
     public void Draw() {
         _bird.Draw();
+
+        foreach (Ground ground in _ground) 
+            ground.Draw();
     }
 
 }
